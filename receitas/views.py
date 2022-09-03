cc95a9044f321a9f0442346b3667a972a876ab29
gellwanger <gustavojce@gmail.com>
2022-09-02 22:28:19 -0300

    Adiciona nome das receitas de forma dinâmica

--- receitas/views.py
+++ receitas/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 
 def index(request):
-  return render(request,'index.html')
+  receitas = {
+    1: 'Aligot de batata',
+    2: 'Macarons au chocolat',
+    3: 'Pain au chocolat',
+    4: 'File au poivre',
+    5: 'Pain perdu',
+    6: 'Petit gâteau',
+  }
+
+  dados = {
+    'nome_das_receitas' : receitas,
+  }
+
+  return render(request, 'index.html', dados)
 
 def receita(request):
   return render(request, 'receita.html')
